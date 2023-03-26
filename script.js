af08070c73148a29995ecd2d8974a3ced109e1c5
MoncoCarser <pjpakkala@gmail.com>
2023-03-26 09:07:37 +0200

    Project code done

---
+++ script.js
@@ -0,0 +1,28 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+// Prompt user to select a mdeia screen, pass video to element, then play
+async function selectMediaStream() {
+	try {
+		const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+		videoElement.srcObject = mediaStream;
+		videoElement.onLoadedmetadata = () => {
+			videoElement.play();
+		}
+	} catch (error) {
+		// Catch errors
+		console.log("whoos, error here:", error)
+	}
+}
+
+button.addEventListener("click", async () => {
+	//  Disable Button
+	button.disabled = true;
+	// Start Picture in Picture
+	await videoElement.requestPictureInPicture();
+	// Reset Button
+	button.disabled = false;
+});
+
+// On load
+selectMediaStream();
